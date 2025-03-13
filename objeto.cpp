c4eea352be6f1354de6c48bb97cf2faca981e160
keren <cardosokerenapuque@gmail.com>
2025-03-12 22:35:37 -0300

    feat: added class object

--- objeto.cpp
+++ objeto.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 
-class Objeto
+
+class Object
 {
+    //
+};
 
+	class StaticObject : public Object
+	{
+		//
+	};
 
-}
+		class Wall : public StaticObject
+		{
+			//
+		};
+
+		class ExitDoor : public StaticObject
+		{
+			//
+		};
+
+	class DynamicObject : public Object
+	{
+		//
+	};
+
+		class Character : public DynamicObject
+		{
+			//
+		};
+
+		class Enemy : public DynamicObject
+		{
+			//
+		};
